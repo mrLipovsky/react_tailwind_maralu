42937e2b7f0d9ac4125d2b7358fff9c12e0e9019
mr_lipovsky <pe.lipovsky@gmail.com>
2023-04-12 21:35:42 +0200

    toggle menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,87 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import { close, logo, menu } from '../assets';
-import { navLinks } from '../constants';
+import { close, logo, menu } from "../assets";
+import { FaBeer, FaMap } from 'react-icons/fa';
+import { ToggleButton } from 'primereact/togglebutton';
 
-const NavBar = () => { 
+
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+  const [active, setActive] = useState("Home");
+  const [toggle, setToggle] = useState(false);
+  const [checked, setChecked] = useState(false);
+
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank"
-        className="w-[124px] h-[32px]"/>
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
-        <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
-          {navLinks.map((nav, index) => (
-            <li key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px]`}
-            >
-            <a href="{`#${nav.id}`}">
-              {nav.title}
-            </a>
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              active === nav.title ? "text-white" : "text-dimWhite"
+            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            onClick={() => setActive(nav.title)}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
 
-            </li>
-          ))}
-        </ul>
+      <div
+          onClick={() => setToggle(!toggle)}
+          className="sm:hidden flex flex-1 justify-end items-center">
+      <FaMap className="w-[28px] h-[28px] object-contain text-white"/>
+      </div>
+
+      {toggle && (
+        <ul className="list-none flex justify-end items-start flex-1 flex-col">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+      )}
+{/* 
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+      <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain text-white"
+          onClick={() => setToggle((prev) => !prev)}
+        /> 
+
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-10 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div> */}
+      {/* </div> */}
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default Navbar;
