1a319c9d633c9bc4a06c041c087e04433f59ee13
mr_lipovsky <pe.lipovsky@gmail.com>
2023-04-13 21:50:25 +0200

    toggle menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 
-import { close, logo, menu } from "../assets";
-import { FaBeer, FaMap } from 'react-icons/fa';
-import { ToggleButton } from 'primereact/togglebutton';
+import { logo } from "../assets";
+import { FaBeer, FaMap, FaPhoneAlt } from 'react-icons/fa';
 
 
 import { navLinks } from "../constants";
@@ -14,7 +13,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      <img src={logo} alt="hoobank" className="w-[60px]" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
@@ -30,56 +29,50 @@ const Navbar = () => {
         ))}
       </ul>
 
-      <div
-          onClick={() => setToggle(!toggle)}
-          className="sm:hidden flex flex-1 justify-end items-center">
-      <FaMap className="w-[28px] h-[28px] object-contain text-white"/>
-      </div>
+      <div className="sm:hidden pr-6 flex flex-1 justify-end items-center">
 
-      {toggle && (
-        <ul className="list-none flex justify-end items-start flex-1 flex-col">
-            {navLinks.map((nav, index) => (
-              <li
-                key={nav.id}
-                className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                  active === nav.title ? "text-white" : "text-dimWhite"
-                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
-                onClick={() => setActive(nav.title)}
-              >
-                <a href={`#${nav.id}`}>{nav.title}</a>
-              </li>
-            ))}
-          </ul>
-      )}
-{/* 
-      <div className="sm:hidden flex flex-1 justify-end items-center">
-      <img
-          src={toggle ? close : menu}
-          alt="menu"
-          className="w-[28px] h-[28px] object-contain text-white"
-          onClick={() => setToggle((prev) => !prev)}
-        /> 
+        <button onClick={() => setToggle((prev) => !prev)}>    
+          {toggle ? 
+            <FaBeer className="w-[28px] h-[28px] text-white"/> 
+            : 
+            <FaMap className="w-[28px] h-[28px] text-white"/> 
+          }
+        </button>
 
         <div
           className={`${
             !toggle ? "hidden" : "flex"
-          } p-6 bg-black-gradient absolute top-10 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          } p-6 absolute top-20 right-0 min-w-[140px] `}
         >
-          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+          <ul className="list-none flex-col">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
-                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                className={`font-poppins text-right font-medium cursor-pointer text-[16px] ${
                   active === nav.title ? "text-white" : "text-dimWhite"
-                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                } ${index === navLinks.length - 1 ? "mb-0" : "mb-2"}`}
                 onClick={() => setActive(nav.title)}
               >
                 <a href={`#${nav.id}`}>{nav.title}</a>
               </li>
+              
             ))}
+            <a rel="stylesheet" href="" className={`font-poppins text-right font-medium cursor-pointer text-[16px] text-white ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                }`} > 
+                <FaPhoneAlt className="w-[16px] text-white/> 
+                +421 904 088 547 
+            </a>
+            <a rel="stylesheet" href=""> 
+              <FaPhoneAlt className="w-[16px] text-white"/> 
+              +421 905 380 345 
+            </a>
           </ul>
-        </div> */}
-      {/* </div> */}
+          <div>
+          
+          </div>
+        </div> 
+      </div>
     </nav>
   );
 };
